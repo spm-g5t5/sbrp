9bcafe52455a859a29b2a72f9f14ecc6d0feb5e5
Jordan Tay <jordantay226@gmail.com>
2023-09-30 15:46:38 +0800

    changed API naming

--- Backend/apply/apply.py
+++ Backend/apply/apply.py
@@ -4,25 +4,25 @@ from models import Apply, ApplySkill
 apply_routes = Blueprint('apply_routes', __name__)
 
 #get all applications
-@apply_routes.route('/api/v1/viewApplicants')
+@apply_routes.route('/API/v1/viewApplicants')
 def viewApplicants():
     applications = Apply.query.all()
     return jsonify([app.json() for app in applications])
 
 #get applications for a specfic role
-@apply_routes.route('/api/v1/viewApplicants/role/<int:id>')
+@apply_routes.route('/API/v1/viewApplicants/role/<int:id>')
 def getApplicantByRoleId(id):
     applications = Apply.query.filter_by(applied_role_id=id)
     return jsonify([app.json() for app in applications])
 
 #get applications for a sepecfic application
-@apply_routes.route('/api/v1/viewApplicants/application/<int:id>')
+@apply_routes.route('/API/v1/viewApplicants/application/<int:id>')
 def getApplicantByApplicationId(id):
     applications = Apply.query.filter_by(application_id=id)
     return jsonify([app.json() for app in applications])
 
 #get skill of a specific application
-@apply_routes.route('/api/v1/viewApplicants/skill/<int:id>')
+@apply_routes.route('/API/v1/viewApplicants/skill/<int:id>')
 def getApplicantBySkillId(id):
     applications = ApplySkill.query.filter_by(application_id=id)
     return jsonify([app.json() for app in applications])
--- Backend/role/role.py
+++ Backend/role/role.py
@@ -4,13 +4,13 @@ from models import Role, RoleSkill
 role_routes = Blueprint('role_routes', __name__)
 
 #get all roles
-@role_routes.route('/api/v1/viewRoles')
+@role_routes.route('/API/v1/viewRoles')
 def viewRoles():
     roles = Role.query.all()
     return jsonify([role.json() for role in roles])
 
 #get skills needed for a specific role
-@role_routes.route('/api/v1/viewRoles/skill/<string:name>')
+@role_routes.route('/API/v1/viewRoles/skill/<string:name>')
 def getSkillsByRoleName(name):
     role_name = RoleSkill.query.filter_by(role_name=name)
     return jsonify([role.json() for role in role_name])
