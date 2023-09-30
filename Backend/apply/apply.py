efaaa3099b44ad2a42e0bf719ba9bc902a70362f
Jordan Tay <jordantay226@gmail.com>
2023-09-30 16:41:15 +0800

    add error handling

--- Backend/apply/apply.py
+++ Backend/apply/apply.py
@@ -6,24 +6,60 @@ apply_routes = Blueprint('apply_routes', __name__)
 #get all applications
 @apply_routes.route('/API/v1/viewApplicants')
 def viewApplicants():
-    applications = Apply.query.all()
-    return jsonify([app.json() for app in applications])
+    try:
+        applications = Apply.query.all()
+        if not applications:
+            # If there are no applications, return a 404 Not Found status
+            return jsonify({"error": "No applicants found"}), 404
+
+        # Return a JSON response with the list of applicants
+        return jsonify([app.json() for app in applications]), 200
+    except Exception as e:
+        # Handle other exceptions (e.g., database errors) with a 500 Internal Server Error
+        return jsonify({"error": str(e)}), 500
 
 #get applications for a specfic role
 @apply_routes.route('/API/v1/viewApplicants/role/<int:id>')
 def getApplicantByRoleId(id):
-    applications = Apply.query.filter_by(applied_role_id=id)
-    return jsonify([app.json() for app in applications])
+    try:
+        applications = Apply.query.filter_by(applied_role_id=id).all()
+        if not applications:
+            # If there are no applicants for the specified role, return a 404 Not Found status
+            return jsonify({"error": "No applicants found for this role"}), 404
+
+        # Return a JSON response with the list of applicants for the specified role
+        return jsonify([app.json() for app in applications]), 200
+    except Exception as e:
+        # Handle other exceptions (e.g., database errors) with a 500 Internal Server Error
+        return jsonify({"error": str(e)}), 500
 
 #get applications for a sepecfic application
 @apply_routes.route('/API/v1/viewApplicants/application/<int:id>')
 def getApplicantByApplicationId(id):
-    applications = Apply.query.filter_by(application_id=id)
-    return jsonify([app.json() for app in applications])
+    try:
+        applications = Apply.query.filter_by(application_id=id).all()
+        if not applications:
+            # If there are no applicants for the specified application ID, return a 404 Not Found status
+            return jsonify({"error": "No applicants found for this application ID"}), 404
+
+        # Return a JSON response with the list of applicants for the specified application ID
+        return jsonify([app.json() for app in applications]), 200
+    except Exception as e:
+        # Handle other exceptions (e.g., database errors) with a 500 Internal Server Error
+        return jsonify({"error": str(e)}), 500
 
 #get skill of a specific application
 @apply_routes.route('/API/v1/viewApplicants/skill/<int:id>')
 def getApplicantBySkillId(id):
-    applications = ApplySkill.query.filter_by(application_id=id)
-    return jsonify([app.json() for app in applications])
+    try:
+        applications = ApplySkill.query.filter_by(application_id=id).all()
+        if not applications:
+            # If there are no applicants for the specified skill ID, return a 404 Not Found status
+            return jsonify({"error": "No applicants found for this skill ID"}), 404
+
+        # Return a JSON response with the list of applicants for the specified skill ID
+        return jsonify([app.json() for app in applications]), 200
+    except Exception as e:
+        # Handle other exceptions (e.g., database errors) with a 500 Internal Server Error
+        return jsonify({"error": str(e)}), 500
 
--- Backend/role/role.py
+++ Backend/role/role.py
@@ -6,11 +6,29 @@ role_routes = Blueprint('role_routes', __name__)
 #get all roles
 @role_routes.route('/API/v1/viewRoles')
 def viewRoles():
-    roles = Role.query.all()
-    return jsonify([role.json() for role in roles])
+    try:
+        roles = Role.query.all()
+        if not roles:
+            # If there are no roles found, return a 404 Not Found status
+            return jsonify({"error": "No roles found"}), 404
+
+        # Return a JSON response with the list of roles
+        return jsonify([role.json() for role in roles]), 200
+    except Exception as e:
+        # Handle other exceptions (e.g., database errors) with a 500 Internal Server Error
+        return jsonify({"error": str(e)}), 500
 
 #get skills needed for a specific role
 @role_routes.route('/API/v1/viewRoles/skill/<string:name>')
 def getSkillsByRoleName(name):
-    role_name = RoleSkill.query.filter_by(role_name=name)
-    return jsonify([role.json() for role in role_name])
+    try:
+        role_name = RoleSkill.query.filter_by(role_name=name).all()
+        if not role_name:
+            # If there are no skills found for the specified role name, return a 404 Not Found status
+            return jsonify({"error": "No skills found for this role name"}), 404
+
+        # Return a JSON response with the list of skills for the specified role name
+        return jsonify([role.json() for role in role_name]), 200
+    except Exception as e:
+        # Handle other exceptions (e.g., database errors) with a 500 Internal Server Error
+        return jsonify({"error": str(e)}), 500
