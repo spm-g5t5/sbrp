fb1bd93970da6262e7f1120453c4b7fe86d3c1cd
Celest-S <113499146+Celest-S@users.noreply.github.com>
2023-10-16 09:08:09 +0800

    Update navbar

--- Frontend/react-app/src/components/Header.tsx
+++ Frontend/react-app/src/components/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
 import logo from '../assets/logo.png';
-import '../styles/Header.css'
-
-
+import '../styles/Header.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function Header() {
+interface HeaderProps {
+  accessRights: number;
+}
 
-    return (
+function Header({ accessRights }: HeaderProps) {
+  return (
     <Navbar expand="lg" className="bgcolor">
       <Container>
         <Navbar.Brand href="#home">
@@ -24,11 +25,20 @@ function Header() {
         <Navbar.Brand className='text-white'>Admin Application Portal</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-
+          {accessRights === 1 ? (
+            // Render the original content when accessRights is 1
+            <div></div> // You can add your additional content here
+          ) : (
+            // Render different content when accessRights is not 1
+            <Nav className="justify-content-end">
+              <Nav.Link href="#role-listing" className="text-white">Role Listing</Nav.Link>
+              <Nav.Link href="#applicants" className="text-white">Applicants</Nav.Link>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 }
 
 export default Header;
