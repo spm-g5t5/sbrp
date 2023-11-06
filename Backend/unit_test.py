53195bf45d0fc1a0b1fd0e398cd1cd1e81714d71
Ren Jie Teo <renjieteo@outlook.com>
2023-11-06 23:55:46 +0800

    Remove mock patch that is unnecessary

--- Backend/unit_test.py
+++ Backend/unit_test.py
@@ -1987,11 +1987,8 @@ class UT_B_UpdateRole(unittest.TestCase):
 
         self.utB003Exp = {"error":"Passed JSON data invalid or missing values, error: \'role_listing_skills\'"}
 
-    @patch('app.db.session.query')
-    @patch('app.db.session.add')
-    @patch('app.db.session.commit')
-    @patch('requests.get')
-    def test_UT_B_001(self, mock_requests_get, db_commit, db_add, mock_query):
+
+    def test_UT_B_001(self):
  
         res = self.app.post(
             "http://127.0.0.1:5000/API/v1/updateRole", json=self.utB001json)
